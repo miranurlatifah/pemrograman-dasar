8841492f8f4cf8e7b3aeb01d79aec8dcd31317ba
miranurlatifah <111743958+miranurlatifah@users.noreply.github.com>
2022-09-19 21:37:03 +0800

    c

---
+++ Dewi Miranda_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama: Dewi Miranda Nurlatifah"<<endl;
+    cout << "Kelas: x PPLG 2"<<endl;
+    cout <<  "90:"<<endl;return 0;
+} 
