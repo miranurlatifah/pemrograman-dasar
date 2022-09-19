e319ffedee05b3d8bc30192ecf0fc04bbbc5a983
miranurlatifah <111743958+miranurlatifah@users.noreply.github.com>
2022-09-19 21:46:04 +0800

    c++/uts

---
+++ Dewi Miranda_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+ 
+int main()
+{
+  string nama;
+  int umur;
+  cout << "nama: ";
+  cin  >> nama;
+  cout << "umur: ";
+  cin  >> umur;
+  cout << " umur berapa " << umur << "tahun" << endl;
+  return 0;
+}
