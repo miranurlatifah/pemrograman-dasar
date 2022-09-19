9939155e46115dc9ae0bb8ee8a75d94d7c452bca
miranurlatifah <111743958+miranurlatifah@users.noreply.github.com>
2022-09-19 21:43:03 +0800

    c++/uts

---
+++ Dewi Miranda_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+ 
+   int jari2,pi;
+   int  luas1, luas2, keliling1, keliling2;
+   jari2 = 27; 
+   pi =22/7;
+   luas1 = pi * jari2;
+   luas2 = luas1 * jari2;
+   cout << "L = pi * jari-jari * jari-jari " << endl;
+   cout << " = " << pi << " * " << jari2 << " * " << jari2 << endl;
+   cout << " = " << luas1 << " * " << jari2 << endl;
+   cout << " = " << luas2 << endl;
+   keliling1 = 2 * pi;
+   keliling2 = keliling1 * jari2;
+   cout << "k = 2 * pi * jari2 " << endl;
+   cout << " = 2 * " << pi << " * " << jari2 << endl;
+   cout << " = " << keliling1 << " * " << jari2 << endl;
+   cout << " = " << keliling2 << endl;
+}   
