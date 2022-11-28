3d024b444e7a09deac7e4fedfea0bd71bcaac738
miranurlatifah <111743958+miranurlatifah@users.noreply.github.com>
2022-11-28 07:18:13 -0700

    Add files via upload

---
+++ assignment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+   int var1, var2 ;
+   int tambah, kurang, kali, bagi ;
+   var1 = 10; // tipe data : integer
+   var2 = 5;
+   cout << "Angka 1=" << var1 << endl;
+   cout << "Angka 2=" << var2 << endl;
+   tambah = var1 + var2 ;
+   cout << "Hasil tambah=" << tambah << endl;
+   kurang = var1 - var2 ;
+   cout << "Hasil pengurangan=" << kurang <<endl;
+   kali = var1 * var2;
+   cout <<"Hasil perkalian=" << kali <<endl;
+   bagi = var1 / var2;
+   cout <<"Hasil pembagian=" << bagi <<endl;
+}
