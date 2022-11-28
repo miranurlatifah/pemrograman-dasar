8fff9d00a8514bc3c6ac44f53e8ecb77c0b5e00b
miranurlatifah <111743958+miranurlatifah@users.noreply.github.com>
2022-11-28 07:26:11 -0700

    c++

---
+++ dewi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama: Dewi Miranda Nurlatifah"<<endl;
+    cout << "Kelas: x PPLG 2"<<endl;return 0;
+}
---
+++ if.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+    int nilai;
+    cout << " nilai kamu = ";
+    cin >> nilai;
+    if (nilai > 78) {
+      cout << "Lulus";
+    }
+    else if (nilai == 78) {
+          cout << "lulus kkm";
+    }
+    else {
+          cout << "tidak lulus kkm";
+    }
+   cout << " nilai tidak valid " << endl;
+    cout <<" nilai valid " <<endl;
+return 0;
+}
