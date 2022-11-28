32bfd57c5fb5c5a7c05d5fbb951c9718a1658489
miranurlatifah <111743958+miranurlatifah@users.noreply.github.com>
2022-11-28 07:19:52 -0700

    c++

---
+++ nested_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+    int a,b,c;
+    
+    for(a=1; a <= 100; a++){
+        for(b=1; b <= a; b++){
+          printf ("*");
+    }
+ printf ("\n");
+}
+}
