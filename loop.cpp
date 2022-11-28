43971fc20dab089f1d2b2c5d6365a96813236eed
miranurlatifah <111743958+miranurlatifah@users.noreply.github.com>
2022-11-28 07:20:56 -0700

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+   int j;
+   for(j= 100; j> 0; j--){
+     printf (" angka %i \n",j);
+   }
+   {
+    int a,b,c
+    for (a= 5 ; a > 0 ; a--){
+    for (b= 1; b <=a ; b++){
+      printf ("*",b); 
+   }
+   printf("\n")
+}
+}
